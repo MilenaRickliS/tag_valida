dfe3b3e1858fca1aa81d60b90ea645524f24f5b6
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2026-01-08 17:03:48 -0300

    menu

--- lib/screens/etiqueta_ativa_detalhes.dart
+++ lib/screens/etiqueta_ativa_detalhes.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
+import '../widgets/menu.dart';
 
 class EtiquetaAtivaDetalhes extends StatelessWidget {
   const EtiquetaAtivaDetalhes({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final w = MediaQuery.of(context).size.width;
+    final compact = w < 835;
     return Scaffold(
+      backgroundColor: const Color(0xFFFDF7ED),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFFDF7ED),
+        elevation: 0,
+        toolbarHeight: compact ? 160 : 100, 
+        centerTitle: true,
+        title: compact
+            ? Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset('assets/logo1.png', height: 78),
+                  const SizedBox(height: 10),
+                  const TopMenu(),
+                ],
+              )
+            : Row(
+                children: [
+                  Image.asset('assets/logo1.png', height: 92),
+                  const Spacer(),
+                  const TopMenu(),
+                ],
+              ),
+      ),
         body: const Center(
           child: Text(
             'Page Etiqueta Ativa Detalhes',
