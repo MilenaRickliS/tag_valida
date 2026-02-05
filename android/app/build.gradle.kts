d64bd10fb953fa4c8116e2088236ea7d74c5ab29
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2026-02-05 15:35:53 -0300

    android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.tag_valida"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.tag_valida"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk 21
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
